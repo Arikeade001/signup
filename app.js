1bb0302c6dcdbe850aff5fd542deec3e6414db46
Arikeade001 <ayoolaadeola409@gmail.com>
2025-02-22 14:42:29 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// const express =require("express");
+
+// const app = express();
+
+
+// app.get("/", (req, res)=> {
+//     res.send("Hello, World!");
+// })
+
+
+// app.get("/profile", (req, res)=> {
+//     res.send("My Profile");
+// })
+
+// app.listen(3000,()=> {
+//     console.log("server is running on port 3000");
+// })
+
+
+
+const express =require("express");
+
+const app = express();
+const env =require("dotenv");
+env.config();
+const mongoose = require ("mongoose");
+const port = process.env.PORT=4000;
+const bcrypt = require("bcrypt");
+
+app.use(express.json())
+
+const connectDB = async ()=> {
+    try{
+       await mongoose .connect("mongodb://localhost:27017/empower") 
+       console.log("mongoDB connected...");
+    }catch (error){
+        console.log ("error connecting to the database");
+    }
+}
+connectDB();
+ 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },  
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:  true,
+        minlength: 5
+    },
+})
+
+ 
+const User = mongoose.model("User", userSchema);
+app.post ("/signUp", async (req, res)=>{
+    try{
+       const{name,email,password}= req.body;
+       const existinguser= await User.findOne({email: email});
+       if(existinguser){
+        return res.status(400).json({msg:"user already exist"});
+       } 
+
+       const hanshedpassword = await bcrypt.hash(password,10)
+       const newuser = new User({
+        name: name,
+        email: email,
+        password: hanshedpassword,
+       })
+       await newuser.save();
+       return res.status(200).json({msg :"user created successfully please log in"})
+    } catch (error){
+        console.log(error);
+        return res.status(200).json({msg:"internal server errornpm run"})
+    }
+})
+
+
+app.post("/login", async (req, res) => {
+    try{
+    const {phoneNumber, password} = req.body
+      
+    if(!phoneNumber  || !password){
+        return res.status.apply(400).json({message: "All filies are required"});
+    }
+    const user = await User.findOne({phoneNumber: phoneNumber});
+
+    if(!user){
+        return res.status(400).json ({message: " user not found"});
+    }
+    const compare = await bcrypt.compare(password, user.password)
+    if(!compare){
+        return res.status(400).json({message: "incorrect password"});
+    }
+    return res.status(200).json({message: "user logged in successfully"});
+ } catch(err){
+    return res.status(500).json({message: "internal server error" })
+ }
+})
+app.get("/backend", (req, res)=> {
+    res.send("Welcome to our Class");
+
+}) 
+
+app.listen(4000,()=> {
+    console.log("server is running on port 4000");
+})
