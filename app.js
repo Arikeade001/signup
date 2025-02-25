7a7be64a129030e0ba053f60cba4f36bcd2f6eee
Arikeade001 <ayoolaadeola409@gmail.com>
2025-02-25 16:34:55 +0100

    initial commit

--- app.js
+++ app.js
@@ -40,37 +40,50 @@ const connectDB = async ()=> {
 connectDB();
  
 const userSchema = new mongoose.Schema({
-    name: {
+    firstName: {
         type: String,
         required: true
-    },  
-    email: {
+    }, 
+    lastName: {
+        type: String,
+        required: true
+    }, 
+    phoneNumber: {
         type: String,
         required: true,
         unique: true
+    },
+    email: {
+        type:String,
+        required:true
     },
     password: {
         type: String,
         required:  true,
         minlength: 5
     },
+    reason : {
+        type: String,
+        required: true
+    }
 })
 
  
 const User = mongoose.model("User", userSchema);
-app.post ("/signUp", async (req, res)=>{
+app.post ("/signup", async (req, res)=>{
     try{
-       const{name,email,password}= req.body;
+       const{firstName, lastName, phoneNumber,email,password,reason}= req.body;
        const existinguser= await User.findOne({email: email});
        if(existinguser){
         return res.status(400).json({msg:"user already exist"});
        } 
 
-       const hanshedpassword = await bcrypt.hash(password,10)
+       const hashedpassword = await bcrypt.hash(password,10)
        const newuser = new User({
-        name: name,
+        firstName: firstName,
+        lastName: lastName,
         email: email,
-        password: hanshedpassword,
+        password: hashedpassword,
        })
        await newuser.save();
        return res.status(200).json({msg :"user created successfully please log in"})
@@ -83,12 +96,12 @@ app.post ("/signUp", async (req, res)=>{
 
 app.post("/login", async (req, res) => {
     try{
-    const {phoneNumber, password} = req.body
+    const {email, password} = req.body
       
-    if(!phoneNumber  || !password){
+    if(!email  || !password){
         return res.status.apply(400).json({message: "All filies are required"});
     }
-    const user = await User.findOne({phoneNumber: phoneNumber});
+    const user = await User.findOne({email: email});
 
     if(!user){
         return res.status(400).json ({message: " user not found"});
@@ -102,8 +115,27 @@ app.post("/login", async (req, res) => {
     return res.status(500).json({message: "internal server error" })
  }
 })
-app.get("/backend", (req, res)=> {
-    res.send("Welcome to our Class");
+
+app.get("/allData", (req, res)=> {
+    User.find({}, (err, users)=>{
+        if(err){
+            return res.status(400).json({msg: "Error getting data"});
+        }
+        res.json(users);
+    })
+})
+
+app.get("/number-of-appplicats", (req, res)=> {
+    User.countDocuments({}, (err, count)=>{
+        if(err){
+            return res.status(400).json({msg: "Error getting data"});
+        }
+        res.json({count: count});
+    })
+})
+
+app.get("/", (req, res)=> {
+    res.send("Welcome to laptop4dev");
 
 }) 
 
